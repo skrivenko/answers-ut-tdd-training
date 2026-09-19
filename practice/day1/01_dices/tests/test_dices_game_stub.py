73bd75bd0f67d1020962e8b4c5d48a175fed4f97
skrivenko <72100114+skrivenko@users.noreply.github.com>
2026-09-19 21:08:38 +0300

    Refactor game_with_bet to use Bet object

--- practice/day1/01_dices/tests/test_dices_game_stub.py
+++ practice/day1/01_dices/tests/test_dices_game_stub.py
@@ -3,21 +3,17 @@ from app import *
 
 def test_player_looses_when_he_played_non_winning_score():
     player = player_with_five_chips()
-    game = game_with_bet(player, 2)
+    game = game_with_bet(player, bet_with_three_chips(2))
     Dice.roll = staticmethod(lambda: 5)
-    
     game.play()
-    
     assert player_has_exactly_chips(player, Chip(2))
 
 
 def test_player_wins_when_he_played_winning_score():
     player = player_with_five_chips()
-    game = game_with_bet(player, 5)
+    game = game_with_bet(player, bet_with_three_chips(5))
     Dice.roll = staticmethod(lambda: 5)
-    
     game.play()
-    
     assert player_has_exactly_chips(player, Chip(2 + 3 * 6))
 
 
@@ -29,11 +25,15 @@ def player_with_five_chips() -> Player:
     return player
 
 
-def game_with_bet(player: Player, score: int) -> RollDiceGame:
+def game_with_bet(player: Player, bet: Bet) -> RollDiceGame:
     game = RollDiceGame()
     player.join(game)
-    game.bet(player, Bet(Chip(3), score))
+    game.bet(player, bet)
     return game
+
+
+def bet_with_three_chips(score: int) -> Bet:
+    return Bet(Chip(3), score)
 
 
 def player_has_exactly_chips(player: Player, chips: Chip) -> bool:
