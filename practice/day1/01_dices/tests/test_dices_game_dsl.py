97eca7bd6bdbe43cfca9040cb5f0d4bbcd91df03
skrivenko <72100114+skrivenko@users.noreply.github.com>
2026-09-20 11:08:30 +0300

    Update test cases to use defined constants
    Refactor tests to use constants for chips and bets.

--- practice/day1/01_dices/tests/test_dices_game_dsl.py
+++ practice/day1/01_dices/tests/test_dices_game_dsl.py
@@ -5,6 +5,12 @@ from app.Exceptions.too_many_players_exception import TooManyPlayersException
 from tests.builders.create import Create
 from tests.builders.create_game import CreateGame
 from tests.builders.create_player import CreatePlayer
+from tests.asserts import Assert
+
+FIVE_CHIPS = Chip(5)
+TWO_CHIPS = Chip(2)
+
+THREE_CHIPS_ON_7 = Bet(Chip(3), 7)
 
 
 def test_player_is_in_game_when_he_joined_game():
@@ -50,9 +56,9 @@ def test_player_can_not_leave_game_if_he_did_not_join_it():
 
 def test_player_has_chips_when_he_bought_them():
     player = Create.player().joined_game(RollDiceGame()).please() 
-    player.buy(Chip(5))
+    player.buy(FIVE_CHIPS)
 
-    assert player_has_exactly_chips(player, Chip(5))
+    Assert.player_has_exactly_chips(player, TWO_CHIPS)
     
 
 def test_player_can_not_bet_if_he_did_not_buy_chips():
@@ -60,7 +66,7 @@ def test_player_can_not_bet_if_he_did_not_buy_chips():
     player = Create.player().joined_game(game).please() 
 
     with pytest.raises(InvalidOperationException) as exception:
-        game.bet(player, Bet(Chip(3), 7))
+        game.bet(player, THREE_CHIPS_ON_7)
 
     assert exception is not None
     
@@ -68,13 +74,9 @@ def test_player_can_not_bet_if_he_did_not_buy_chips():
 def test_player_looses_when_he_played_non_existing_score():
     game = RollDiceGame()
     player = Create.player().joined_game(game).with_five_chips().please() 
+    game.bet(player, THREE_CHIPS_ON_7)
     
-    game.bet(player, Bet(Chip(3), 7))
     game.play()
 
-    assert player_has_exactly_chips(player, Chip(2))
+    Assert.player_has_exactly_chips(player, TWO_CHIPS)
 
-
-### private function for assert
-def player_has_exactly_chips(player: Player, chips: Chip) -> bool:
-    return player.has(chips) and not player.has(chips + Chip(1))  
